9db5eee43d62c6e3f57323f86926bb2835846057
Jeffrey Lederer <jeffrey@winnlederer.com>
2025-03-11 13:24:41 -0400

    edit changes

--- reacttype1.server.testing/SetupMembershipRepository.cs
+++ reacttype1.server.testing/SetupMembershipRepository.cs
@@ -5,38 +5,31 @@ using ReactType1.Server.Repository;
 
 public class SetupMembershipRepository : IDisposable
 {
-    private DbLeagueApp context;
+    private DbLeagueApp? context;
+
     public  IMembershipRepository GetRepository()
     {
-
         var builder = new DbContextOptionsBuilder<DbLeagueApp>();
-        builder.UseInMemoryDatabase(databaseName: "LibraryDbInMemory");
+        builder.UseInMemoryDatabase(databaseName: "TestDbInMemory");
 
         var dbContextOptions = builder.Options;
         context = new DbLeagueApp(dbContextOptions);
 
-        // Insert seed data into the database using one instance of the context
-        
-            if (context.Memberships.Count() == 0)
-            {
-                context.Memberships.Add(new Membership { Id = 1, FirstName = "John", LastName = "Doe", FullName = "John Doe", Shortname = "JD", NickName = "Johnny", Wheelchair = false });
-                context.Memberships.Add(new Membership { Id = 2, FirstName = "Jack", LastName = "Doe", FullName = "John Doe", Shortname = "JD", NickName = "Johnny", Wheelchair = false });
-                context.Memberships.Add(new Membership { Id = 3, FirstName = "Mary", LastName = "Doe", FullName = "John Doe", Shortname = "JD", NickName = "Johnny", Wheelchair = false });
-                context.SaveChanges();
-            }
-
-
-
+        // Insert seed data into the database using instance of the context
+        context.Memberships.Add(new Membership { Id = 1, FirstName = "John", LastName = "Doe", FullName = "John Doe", Shortname = "JD", NickName = "Johnny", Wheelchair = false });
+        context.Memberships.Add(new Membership { Id = 2, FirstName = "Jack", LastName = "Doe", FullName = "John Doe", Shortname = "JD", NickName = "Johnny", Wheelchair = false });
+        context.Memberships.Add(new Membership { Id = 3, FirstName = "Mary", LastName = "Doe", FullName = "John Doe", Shortname = "JD", NickName = "Johnny", Wheelchair = false });
+        context.SaveChanges();
 
         // Use a clean instance of the context to run the test
-
         IMembershipRepository repository = new MembershipRepository(context);
         return repository;
     }
 
     public void Dispose()
     {
-        context.Database.EnsureDeleted(); 
+        if(context != null)
+         context.Database.EnsureDeleted(); 
     }   
 
 }
