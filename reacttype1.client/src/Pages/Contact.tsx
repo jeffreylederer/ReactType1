6bf9dd8171bc5117b6dacf8351e8c76e71b33843
Jeffrey Lederer <jeffrey@winnlederer.com>
2025-03-31 10:32:15 -0400

    updated contact

--- reacttype1.client/src/Pages/Contact.tsx
+++ reacttype1.client/src/Pages/Contact.tsx
@@ -6,7 +6,7 @@ const Contact = () => {
             <h3 style={{ textAlign: 'center' }}>Contact</h3>
 
             <p style={{ width: '100%', textAlign: 'left' }}>This application is supported by Jeffrey Lederer.
-            The source code depository for this application as at <a href="https://github.com/jeffreylederer/ReactType" target="blank">https://github.com/jeffreylederer/ReactType</a>
+            The source code depository for this application as at <a href="https://github.com/jeffreylederer/ReactType1" target="blank">https://github.com/jeffreylederer/ReactType</a>
          </p>
 
                 <p style={{ width: '100%', textAlign: 'left' }}>
