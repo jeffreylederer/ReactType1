ead5f29b4f25b11960a76028336fc3e47691f98c
Jeffrey Lederer <jeffrey@winnlederer.com>
2025-04-01 17:51:00 -0400

    remove types

--- reacttype1.client/src/pages/Admin/Login/FormData.tsx
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-
-
-export const FormDataSchema = z.object({
-    username: z.string().min(1, "User name is required").max(450,"User name is limited to 450 characters"),
-    password: z.string().min(1, "Password is required").max(50, "Password is limited to 450 characters"),
-});
-
-export type FormData = z.infer<typeof FormDataSchema>;
