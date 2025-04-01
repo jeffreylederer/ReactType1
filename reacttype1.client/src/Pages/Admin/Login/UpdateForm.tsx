fee030ef02e259d3a52ea80c81dd59e9c0e438f3
Jeffrey Lederer <jeffrey@winnlederer.com>
2025-04-01 17:50:47 -0400

    remove types

--- reacttype1.client/src/Pages/Admin/Login/UpdateForm.tsx
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-
-
-export const UpdateFormDataSchema = z.object({
-    id: z.coerce.number(),
-    password: z.string().email("Must be an email address").min(1, "Password is required").max(50, "Password is limited to 450 characters"),
-});
-
-export type UpdateFormData = z.infer<typeof UpdateFormDataSchema>;
--- reacttype1.client/src/Pages/Admin/Login/UserTypeDetail.tsx
+++
@@ -1,5 +0,0 @@
-export type UserTypeDetail  = {
-    id: number,
-    username: string,
-    role: string
-}
