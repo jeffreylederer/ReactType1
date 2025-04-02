000100c16aba96452a75efa7beaec9f60c1bff9e
Jeffrey Lederer <jeffrey@winnlederer.com>
2025-04-02 18:20:34 -0400

    again

--- reacttype1.client/src/pages/About.tsx
+++ reacttype1.client/src/pages/About.tsx
@@ -1,19 +1,46 @@
-
 import Layout from "@layouts/Layout.tsx";
+import { SiteInfoType } from './SiteInfoType.ts';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 
 
 const About = () => {
-    return (
-        <Layout >
+    const [data, setData] = useState<SiteInfoType>();
 
-            <h3 style={{textAlign: 'center'} }>About</h3>
+    useEffect(() => {
+        GetData();
+    });
+
+
+    const contents = data === undefined
+        ? <p><em>Loading ...</em></p>
+        :
+        <>
+
+            <h3 style={{ textAlign: 'center' }}>About</h3>
 
             <p style={{ width: '100%', textAlign: 'left' }}>This application is used to track leagues at
-                the Frick Park Lawn Bowling Club.</p>
+                the {data.clubname}</p>
 
             <p style={{ width: '100%', textAlign: 'left' }}>The FPLBC website is <a href="http://lawnbowlingpittsburgh.org">http://lawnbowlingpittsburgh.org</a></p>
+        </>;
+
+    return (
+        <Layout>
+            {contents}
         </Layout>
     );
+
+    function GetData() {
+        axios.get(import.meta.env.VITE_SERVER_URL + "api/home")
+            .then(response => {
+                setData(response.data);
+            })
+            .catch(error => {
+                console.error('Error fetching data: ', error);
+            })
+    }
 }
 
 
