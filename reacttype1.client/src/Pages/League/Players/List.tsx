4fb4a2aef64bf0e012c65eb915bcb6ce3263abf4
Jeffrey Lederer <jeffrey@winnlederer.com>
2025-04-08 10:43:51 -0400

    again

--- reacttype1.client/src/Pages/League/Players/List.tsx
+++ reacttype1.client/src/Pages/League/Players/List.tsx
@@ -3,41 +3,48 @@ import  UpdateFormData from "./UpdateFormData.tsx";
 import { User, League } from "@components/leagueObject.tsx";;
 import Layout from '@layouts/Layout.tsx';
 import Table from './Table.tsx';
-import useFetch from '@hooks/useFetch.tsx';
+import { useState, useEffect } from 'react';
 
 function Players() {
    
     const permission: string = User().role;
     const allowed: boolean = (permission == "SiteAdmin" || permission == "Admin") ? false : true;
-    const { data, loading, error } = useFetch<UpdateFormData>(`${import.meta.env.VITE_SERVER_URL}api/players/${League().id}`);
+    const [data, setData] = useState<UpdateFormData[]>();
+    const [error, setError] = useState<string>('');
 
-    if (loading) {
-        return <p>Loading...</p>;
+    const fetchData = async () => {
+        try {
+            const response = await fetch(`${import.meta.env.VITE_SERVER_URL}api/players/${League().id}`);
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            const json = (await response.json()) as UpdateFormData[];
+            setData(json);
+
+        } catch (error) {
+            let message: string;
+            if (error instanceof Error)
+                message = error.message
+            else
+                message = String(error)
+            setError(message);
+        }
     }
 
-    if (error)
-        return <p>Error: {error}</p>;
+        useEffect(() => {
+            fetchData();
+        },[data]);
 
-    if (!data)
-        return (
-
-            <Layout>
-
-                <h3>Players in league {League().leagueName}</h3>
-                <Link to="/League/Players/Create" hidden={allowed}>Add</Link>
-                <p>No Players</p>
-            </Layout>
-        );
-
-     
+        const content = data === undefined ? <p>Loading...</p> : <Table data={data} rowsPerPage={15} allowed={allowed} />;
 
     return (
         <Layout>
 
             <h3>Players in league {League().leagueName}</h3>
             <Link to="/League/Players/Create" hidden={allowed}>Add</Link>
-            <Table data={data} rowsPerPage={15} allowed={allowed} />
-            <p>Number of players: {data.length}</p>
+            {content }
+            <p>Number of players: {data && data.length}</p>
+            <p>{error}</p>
         </Layout>
     );
 
