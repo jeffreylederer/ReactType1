0ec969245c27a6b8875ba655568585088eca1f8f
Jeffrey Lederer <jeffrey@winnlederer.com>
2025-04-28 12:00:55 -0400

    Delete reacttype1.client/src/Pages directory

--- reacttype1.client/src/Pages/League/Matches/Playoffs.tsx
+++
@@ -1,8 +0,0 @@
-export const Playoffs = () => {
-    return (
-        <>
-            <h3>Playoffs</h3>
-            <p>not written yet</p>
-        </>
-    );
-}
--- reacttype1.client/src/Pages/League/Teams/Membership.tsx
+++
@@ -1,8 +0,0 @@
-export type Membership ={
-    id: number,
-    firstName: string,
-    lastName: string,
-    fullName: string,
-    shortname: string
-    wheelchair: boolean
-};
