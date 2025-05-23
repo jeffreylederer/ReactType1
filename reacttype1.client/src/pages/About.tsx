19bdffe5e7fc06244e2ae42c71d640065acf98f8
Jeffrey Lederer <jeffrey@winnlederer.com>
2025-05-23 15:12:38 -0400

    updated about with new name

--- reacttype1.client/src/pages/About.tsx
+++ reacttype1.client/src/pages/About.tsx
@@ -18,7 +18,7 @@ const About = () => {
             <p style={{ width: '100%', textAlign: 'left' }}>The club's website is <a href={import.meta.env.VITE_SERVER_Website}>{import.meta.env.VITE_SERVER_Website}</a></p>
 
             <p style={{ width: '100%', textAlign: 'left' }}>
-                The source code depository for this application as at <a href="https://github.com/jeffreylederer/ReactType1" target="blank">https://github.com/jeffreylederer/ReactType1</a>
+                The source code depository for this application as at <a href="https://github.com/jeffreylederer/BowlingLeagueApp" target="blank">https://github.com/jeffreylederer/BowlingLeagueApp</a>
             </p>
         </Layout>
     );
