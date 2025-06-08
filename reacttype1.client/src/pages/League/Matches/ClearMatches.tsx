64c345edfb6d31170dbc3ff1fa0d1261822b09d6
Jeffrey Lederer <jeffrey@winnlederer.com>
2025-06-08 19:06:34 -0400

    fixed

--- reacttype1.client/src/pages/League/Matches/ClearMatches.tsx
+++ reacttype1.client/src/pages/League/Matches/ClearMatches.tsx
@@ -1,26 +1,40 @@
+import { useEffect, useState } from 'react';
+import axios from "axios";
 import LeagueClass from "@components/LeagueClass.tsx";
 import Layout from '@layouts/Layout.tsx';
-import useFetch from '@hooks/useFetch.tsx';
+import { SetCount } from '@components/CountMatches.tsx';
 
 
 const ClearMatches = () => {
     const league = new LeagueClass();
 
-    const { data, isLoading, error } = useFetch<string>(`/api/Matches/ClearSchedule/${league.id}`);
-    if (isLoading)
-        return <p aria-label="Loading">Loading...</p>;
-
-    if (error)
-        return <p aria-label="Error">Return Error: {error}</p>;
-    
+    const [errorMsg, setErrorMsg] = useState('');
+    useEffect(() => {
+        GetData();
+    });
     return (
         <Layout>
             <h3>Clear Matches</h3>
-            <p style={{ textAlign: "center"} }>{data}</p>
+            <p style={{ textAlign: "center" }}>{errorMsg}</p>
         </Layout>
     );
-  
-   
+
+    async function GetData() {
+        const url: string = "/api/Matches/ClearSchedule/".concat(league.id.toString());
+        axios.get(url)
+            .then(response => {
+                setErrorMsg("Schedule cleared");
+                SetCount(0);
+                console.log(response.data);
+
+            })
+            .catch(error => {
+                if (error.response.status === 404)
+                    setErrorMsg("Could not find service")
+                else
+                    setErrorMsg(error.response.data);
+            })
+    }
 }
 
 export default ClearMatches;
--- reacttype1.client/src/pages/League/Matches/CreateMatches.tsx
+++ reacttype1.client/src/pages/League/Matches/CreateMatches.tsx
@@ -1,24 +1,37 @@
-import LeagueClass from "@components/LeagueClass.tsx";
+import { useEffect, useState } from 'react';
+import axios from "axios";
 import Layout from '@layouts/Layout.tsx';
-import useFetch from '@hooks/useFetch.tsx';
+import LeagueClass from "@components/LeagueClass";;
+
+import { SetCount } from '@components/CountMatches.tsx';
 
 const CreateMatches = () => {
-   
+    const [errorMsg, setErrorMsg] = useState('Matches created');
     const league = new LeagueClass();
-    const { data, isLoading, error } = useFetch<string>(`/api/Matches/CreateSchedule/${league.id}`);
-    if (isLoading)
-        return <p aria-label="Loading">Loading...</p>;
-
-    if (error)
-        return <p aria-label="Error">Return Error: {error}</p>;
+    useEffect(() => {
+        GetData();
+    });
     return (
         <Layout>
             <h3>Create Schedule</h3>
-            <p style={{ textAlign: "center" }}>{data}</p>
+            <p style={{ textAlign: "center" }}>{errorMsg}</p>
         </Layout>
     );
 
-   
+    async function GetData() {
+        const url: string = "/api/Matches/CreateSchedule/".concat(league.id.toString());
+        axios.get(url)
+            .then(response => {
+                SetCount(1);
+                console.log(response.data);
+            })
+            .catch(error => {
+                if (error.response.status === 404)
+                    setErrorMsg("Could not find service")
+                else
+                    setErrorMsg(error.response.data);
+            })
+    }
 }
 
 export default CreateMatches;
